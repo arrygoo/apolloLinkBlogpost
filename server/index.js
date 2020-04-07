885699e2dd893a9db64c95153ce3dd3c9328efd3
Aryan Goharzad <arrygoo@gmail.com>
2020-04-07 10:53:54 -0400

    blogpost

---
+++ server/index.js
@@ -0,0 +1,76 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  type User {
+    id: ID!
+    name: String
+    planetId: ID
+  }
+
+  type Planet {
+    id: String
+    name: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+    planet(planetId: ID!): Planet
+    user: User
+  }
+`;
+
+const planets = [
+  { id: 0, name: "jupiter" },
+  { id: 1, name: "earth" },
+  { id: 2, name: "mars" }
+];
+
+const userProfile = {
+  id: "123",
+  name: "Aryan",
+  planetId: "1"
+};
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+    planet: (parent, { planetId }) => planets[planetId],
+    user: () => userProfile
+  }
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
